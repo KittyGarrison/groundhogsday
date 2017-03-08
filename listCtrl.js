cd2d3ba16a5c7a270f2b3f8d7651e026b563621b
KittyGarrison <katrina.garrison@detroitlabs.com>
2017-03-08 12:00:35 -0500

    made a factory

--- listCtrl.js
+++ listCtrl.js
@@ -6,12 +6,17 @@ angular
     var vm = this;
     vm.title = 'Todo list';
     vm.taskList = [];
-    vm.taskText = '';
+    vm.taskName = '';
     vm.addTask = addTask;
+    vm.removeTask = removeTask;
+
+    function removeTask(task){
+      var index = vm.taskList.indexOf(task);
+      vm.taskList.splice(index,1);
+    };
 
     function addTask(task){
-      vm.taskList.push(task);
-      vm.taskText = "";
-      console.log(vm.taskList);
-    }
+      vm.taskList.push(taskFactory.create(task));
+      vm.taskName = "";
+    };
   }
--- taskFactory.js
+++ taskFactory.js
@@ -3,10 +3,19 @@ angular
   .factory('taskFactory', taskFactory);
 
   function taskFactory(){
+
+    function create(taskName){
+      return new Task(taskName);
+    };
+
+
     function Task(name){
       this.name = name;
       this.isDone = false;
+      this.finish = function(){
+        this.isDone = !this.isDone;
+      };
     };
 
-    return Task;
+    return {create: create};
   }
