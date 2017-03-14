4ef396fcd917b03b39277ebdc447ffcb306beb92
KittyGarrison <katrina.garrison@detroitlabs.com>
2017-03-13 22:07:14 -0400

    my friend came by so I made her into an object literal

--- listController.js
+++ listController.js
@@ -4,5 +4,29 @@ angular
 
   function listCtrl(){
     var vm = this;
-    vm.message = "Todo";
+    vm.message = 'Todo';
+    vm.list = [];
+    vm.taskName = '';
+    vm.addTask = addTask;
+    vm.removeTask = removeTask;
+    vm.friend = {
+      name:"Lilli",
+      hairColor:"brown",
+      newColor:"new color",
+      changeHair: function(color){
+        console.log(color);
+        this.hairColor = color
+      }
+    }
+
+
+    function addTask(name){
+      vm.list.push(name);
+      vm.taskName = '';
+    }
+
+    function removeTask(task) {
+      var index = vm.list.indexOf(task);
+      vm.list.splice(index,1);
+    }
   }
